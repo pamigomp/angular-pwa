840940f8a81b0086f3fba844342b5782b58480d5
Michal Pietrzak <201011@student.pwr.wroc.pl>
2019-08-19 22:49:34 +0200

    Add service templates

---
+++ src/app/services/cart/cart.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CartModel } from '../../../models/cart.model';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { ConfigService } from '../config/config.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+  private SERVER_API_URL: string;
+
+  constructor(private httpService: HttpClient, private configService: ConfigService) {
+   this.SERVER_API_URL = this.configService.get('API_URL');
+  }
+
+  public getCart(id: number): Observable<CartModel> {
+    return this.httpService.get<CartModel>(`${this.SERVER_API_URL}/carts/${id}`).pipe(
+      map(data => new CartModel().deserialize(data)),
+      catchError(() => throwError('User not found'))
+    );
+  }
+
+  public getAllCarts(): Observable<CartModel[]> {
+    return this.httpService.get<CartModel[]>(`${this.SERVER_API_URL}/carts`).pipe(
+      map(data => data.map(item => new CartModel().deserialize(item)))
+    );
+  }
+}
