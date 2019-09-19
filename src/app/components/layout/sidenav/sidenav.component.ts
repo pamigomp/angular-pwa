1871647089efab5b0abf6c6d5d275fbf1acced74
Michal Pietrzak <201011@student.pwr.wroc.pl>
2019-09-19 23:32:06 +0200

    Update sidebar component content.

--- src/app/components/layout/sidenav/sidenav.component.ts
+++ src/app/components/layout/sidenav/sidenav.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoryModel } from '../../../models/category.model';
+import { Observable } from 'rxjs';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { map, share } from 'rxjs/operators';
+import { CategoryService } from '../../../services/category/category.service';
 
 @Component({
   selector: 'app-sidenav',
@@ -6,11 +11,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidenav.component.scss']
 })
 export class SidenavComponent implements OnInit {
+  categories: CategoryModel[];
 
-  constructor() {
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      share()
+    );
+
+  constructor(private categoryService: CategoryService, private breakpointObserver: BreakpointObserver) {
   }
 
   ngOnInit() {
+    this.categoryService.getAllCategories().subscribe((categories: CategoryModel[]) => {
+      this.categories = categories;
+    });
   }
-
 }
